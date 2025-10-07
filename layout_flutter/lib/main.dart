da3e20030b175e9e34533256d3c48d3c7557a567
Lavina-23 <lavinakohai3227@gmail.com>
2025-10-08 05:20:09 +0700

    feat: button row implementation

--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -45,6 +45,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Lavina 2341760062',
       home: Scaffold(
@@ -54,8 +65,30 @@ class MyApp extends StatelessWidget {
           body: Column(
             children: [
               titleSection,
+              buttonSection,
             ],
           )),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
